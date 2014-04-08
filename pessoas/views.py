0ae9e2297c32c47ec4cbaded40b414a855ebcc21
Alex Falcucci <alex.falcucci@hotmail.com>
2014-04-07 21:48:31 -0300

    Colocando o formulário!

--- pessoas/views.py
+++ pessoas/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render, HttpResponseRedirect
 from pessoas.models import Pessoa
 
 def index(request):
-   	return render(request, 'index.html')
+    return render(request, 'index.html')
 
 def pessoaListar(request):
-    #pessoas = Pessoa.objects.all()[0:10]
-    pessoas = []
-    pessoas.append(Pessoa(nome='UNIFRAN', email='EMAIL', telefone='(35) 3544-1656'))
-    pessoas.append(Pessoa(nome='CRUZEIRO'))
+    pessoas = Pessoa.objects.all()[0:10]
+    #pessoas = []
+    #pessoas.append(Pessoa(nome='UNIFRAN', email='EMAIL', telefone='(35) 3544-1656'))
+    #pessoas.append(Pessoa(nome='CRUZEIRO'))
 
     return render(request, 'pessoas/listaPessoas.html', {'pessoas': pessoas})
 
 def pessoaAdicionar(request):
-	return render(request, 'pessoas/formPessoas.html)
+    return render(request, 'pessoas/formPessoas.html')
